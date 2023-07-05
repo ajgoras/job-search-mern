66d83970dc8414bf13cc4addfd24c8a00eb2daef
ajgoras <igorjonczyk99@gmail.com>
2023-07-05 13:12:14 +0200

    blocked register modal when user is logged in

--- src/components/RegisterModal.tsx
+++ src/components/RegisterModal.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useContext, useEffect, useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import AlertModal from "./AlertModal";
 import "../styles/style.css";
@@ -7,6 +7,10 @@ import axios from "axios";
 import { checkFormValidity } from "../functions/checkFormValidity";
 import { axiosUrls } from "../axiosUrls/axiosUrls";
 import { useNavigate } from "react-router-dom";
+import { ContextsType } from "../types/ContextsType";
+import { Contexts } from "../contexts/Contexts";
+import { defaultLoggedUserState } from "../types/LoggedUser";
+import { Button } from "react-bootstrap";
 
 function RegisterModal(props: any) {
   const [password, setPassword] = useState("");
@@ -20,6 +24,8 @@ function RegisterModal(props: any) {
   const [placeHoldersVisibility, setPlaceHoldersVisibility] = useState<
     boolean[]
   >([]);
+
+  const { loggedUser }: ContextsType = useContext(Contexts);
 
   const navigate = useNavigate();
   const [show, setShow] = useState(false);
@@ -87,133 +93,166 @@ function RegisterModal(props: any) {
         }}
       >
         <Modal.Title id="contained-modal-title-vcenter">
-          Create company account
+          {loggedUser === defaultLoggedUserState ? (
+            <div>Create company account</div>
+          ) : (
+            <div>Already logged as {loggedUser.company_name}</div>
+          )}
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <form id="register-form" onSubmit={registerSubmit}>
-          <div className="form-section">
-            <input
-              required
-              type="email"
-              title="EmailInput"
+        {loggedUser === defaultLoggedUserState ? (
+          <form id="register-form" onSubmit={registerSubmit}>
+            <div className="form-section">
+              <input
+                required
+                type="email"
+                title="EmailInput"
+                className={
+                  email === "" ? "form-input" : "form-input form-input-filled"
+                }
+                name="EmailInput"
+                onChange={(event) => setEmail(event.target.value)}
+                value={email}
+                placeholder={
+                  placeHoldersVisibility[0]
+                    ? "Enter your company email address"
+                    : ""
+                }
+                onFocus={() => {
+                  const newArr = [...placeHoldersVisibility];
+                  newArr[0] = true;
+                  setPlaceHoldersVisibility(newArr);
+                }}
+                onBlur={() => {
+                  setPlaceHoldersVisibility([]);
+                }}
+              />
+              <label htmlFor="EmailInput" className="input-label">
+                <span className="label-name">Email Address</span>
+              </label>
+            </div>
+            <div className="form-section">
+              <input
+                required
+                type="password"
+                name="PasswordInput"
+                title="PasswordInput"
+                className="form-input"
+                onChange={(event) => setPassword(event.target.value)}
+                value={password}
+                placeholder={
+                  placeHoldersVisibility[1] ? "Enter your password" : ""
+                }
+                onFocus={() => {
+                  const newArr = [...placeHoldersVisibility];
+                  newArr[1] = true;
+                  setPlaceHoldersVisibility(newArr);
+                }}
+                onBlur={() => {
+                  setPlaceHoldersVisibility([]);
+                }}
+              />
+              <label htmlFor="PasswordInput" className="input-label">
+                <span className="label-name">Password</span>
+              </label>
+            </div>
+            <div className="form-section">
+              <input
+                required
+                type="text"
+                name="CompanyNameInput"
+                title="CompanyNameInput"
+                className="form-input"
+                onChange={(event) => setCompanyName(event.target.value)}
+                value={companyName}
+                placeholder={
+                  placeHoldersVisibility[2]
+                    ? "Enter your full company name"
+                    : ""
+                }
+                onFocus={() => {
+                  const newArr = [...placeHoldersVisibility];
+                  newArr[2] = true;
+                  setPlaceHoldersVisibility(newArr);
+                }}
+                onBlur={() => {
+                  setPlaceHoldersVisibility([]);
+                }}
+              />
+              <label htmlFor="CompanyNameInput" className="input-label">
+                <span className="label-name">Full Company Name</span>
+              </label>
+            </div>
+            <div className="form-section">
+              <input
+                required
+                type="url"
+                name="LogoInput"
+                title="LogoInput"
+                className={
+                  logo === "" ? "form-input" : "form-input form-input-filled"
+                }
+                onChange={(event) => setLogo(event.target.value)}
+                value={logo}
+                placeholder={
+                  placeHoldersVisibility[3]
+                    ? "Paste your company logo link"
+                    : ""
+                }
+                onFocus={() => {
+                  const newArr = [...placeHoldersVisibility];
+                  newArr[3] = true;
+                  setPlaceHoldersVisibility(newArr);
+                }}
+                onBlur={() => {
+                  setPlaceHoldersVisibility([]);
+                }}
+              />
+              <label htmlFor="LogoInput" className="input-label">
+                <span className="label-name">Company Logo Link</span>
+              </label>
+            </div>
+            <div className="form-group form-check"></div>
+            <button
+              type="submit"
+              title="submitButton"
               className={
-                email === "" ? "form-input" : "form-input form-input-filled"
+                shake
+                  ? "shake"
+                  : "gradient-button submit-button btn btn-primary"
               }
-              name="EmailInput"
-              onChange={(event) => setEmail(event.target.value)}
-              value={email}
-              placeholder={
-                placeHoldersVisibility[0]
-                  ? "Enter your company email address"
-                  : ""
-              }
-              onFocus={() => {
-                const newArr = [...placeHoldersVisibility];
-                newArr[0] = true;
-                setPlaceHoldersVisibility(newArr);
+              onClick={() => {
+                if (
+                  !checkFormValidity({ email, password, companyName, logo })
+                ) {
+                  startShake();
+                }
               }}
-              onBlur={() => {
-                setPlaceHoldersVisibility([]);
-              }}
-            />
-            <label htmlFor="EmailInput" className="input-label">
-              <span className="label-name">Email Address</span>
-            </label>
+            >
+              Create Account
+            </button>
+          </form>
+        ) : (
+          <div>
+            <div className="warning-info-div">
+              <p>You are already logged into your account</p>
+            </div>
+            <div className="warning-info-div">
+              <Button
+                onClick={() => {
+                  setShow(false);
+                  setTimeout(() => {
+                    navigate("/");
+                  }, 100);
+                }}
+              >
+                OK
+              </Button>
+            </div>
           </div>
-          <div className="form-section">
-            <input
-              required
-              type="password"
-              name="PasswordInput"
-              title="PasswordInput"
-              className="form-input"
-              onChange={(event) => setPassword(event.target.value)}
-              value={password}
-              placeholder={
-                placeHoldersVisibility[1] ? "Enter your password" : ""
-              }
-              onFocus={() => {
-                const newArr = [...placeHoldersVisibility];
-                newArr[1] = true;
-                setPlaceHoldersVisibility(newArr);
-              }}
-              onBlur={() => {
-                setPlaceHoldersVisibility([]);
-              }}
-            />
-            <label htmlFor="PasswordInput" className="input-label">
-              <span className="label-name">Password</span>
-            </label>
-          </div>
-          <div className="form-section">
-            <input
-              required
-              type="text"
-              name="CompanyNameInput"
-              title="CompanyNameInput"
-              className="form-input"
-              onChange={(event) => setCompanyName(event.target.value)}
-              value={companyName}
-              placeholder={
-                placeHoldersVisibility[2] ? "Enter your full company name" : ""
-              }
-              onFocus={() => {
-                const newArr = [...placeHoldersVisibility];
-                newArr[2] = true;
-                setPlaceHoldersVisibility(newArr);
-              }}
-              onBlur={() => {
-                setPlaceHoldersVisibility([]);
-              }}
-            />
-            <label htmlFor="CompanyNameInput" className="input-label">
-              <span className="label-name">Full Company Name</span>
-            </label>
-          </div>
-          <div className="form-section">
-            <input
-              required
-              type="url"
-              name="LogoInput"
-              title="LogoInput"
-              className={
-                logo === "" ? "form-input" : "form-input form-input-filled"
-              }
-              onChange={(event) => setLogo(event.target.value)}
-              value={logo}
-              placeholder={
-                placeHoldersVisibility[3] ? "Paste your company logo link" : ""
-              }
-              onFocus={() => {
-                const newArr = [...placeHoldersVisibility];
-                newArr[3] = true;
-                setPlaceHoldersVisibility(newArr);
-              }}
-              onBlur={() => {
-                setPlaceHoldersVisibility([]);
-              }}
-            />
-            <label htmlFor="LogoInput" className="input-label">
-              <span className="label-name">Company Logo Link</span>
-            </label>
-          </div>
-          <div className="form-group form-check"></div>
-          <button
-            type="submit"
-            title="submitButton"
-            className={
-              shake ? "shake" : "gradient-button submit-button btn btn-primary"
-            }
-            onClick={() => {
-              if (!checkFormValidity({ email, password, companyName, logo })) {
-                startShake();
-              }
-            }}
-          >
-            Create Account
-          </button>
-        </form>
+        )}
+
         <AlertModal
           show={alertModalShow}
           onHide={() => {
