c33ef53cc513ac4c2e62bd0c7b8a42ad9090ced6
ajgoras <igorjonczyk99@gmail.com>
2023-07-05 15:04:13 +0200

    fixed not working login modal test

--- src/tests/LoginModal.test.tsx
+++ src/tests/LoginModal.test.tsx
@@ -6,6 +6,36 @@ import LoginModal from "../components/LoginModal";
 import { BrowserRouter } from "react-router-dom";
 
 describe("Login Modal Test", () => {
+  test("should not login to an account that does not exist", async () => {
+    let showLoginModal = true;
+    render(
+      <ContextsProvider>
+        <BrowserRouter>
+          <LoginModal
+            show={showLoginModal}
+            onHide={() => {
+              showLoginModal = false;
+            }}
+          ></LoginModal>
+        </BrowserRouter>
+      </ContextsProvider>
+    );
+
+    const loginData = {
+      email: "adminnn@adminnn.comr",
+      password: "adminnn",
+    };
+
+    const loginInput = await screen.findByTitle("EmailInput");
+    const passwordInput = await screen.findByTitle("PasswordInput");
+    fireEvent.change(loginInput, { target: { value: loginData.email } });
+    fireEvent.change(passwordInput, { target: { value: loginData.password } });
+    const submitButton = screen.getByTitle("submitButton");
+    fireEvent.click(submitButton);
+    const errorInfo = await screen.findByText("Bad login or password!");
+    expect(errorInfo).toBeInTheDocument();
+  });
+
   test("should login as admin", async () => {
     let showLoginModal = true;
     render(
@@ -38,35 +68,5 @@ describe("Login Modal Test", () => {
       "Successfully logged as admin@admin.com"
     );
     expect(successInfo).toBeInTheDocument();
-  });
-
-  test("should not login to an account that does not exist", async () => {
-    let showLoginModal = true;
-    render(
-      <ContextsProvider>
-        <BrowserRouter>
-          <LoginModal
-            show={showLoginModal}
-            onHide={() => {
-              showLoginModal = false;
-            }}
-          ></LoginModal>
-        </BrowserRouter>
-      </ContextsProvider>
-    );
-
-    const loginData = {
-      email: "adminnn@adminnn.comr",
-      password: "adminnn",
-    };
-
-    const loginInput = await screen.findByTitle("EmailInput");
-    const passwordInput = await screen.findByTitle("PasswordInput");
-    fireEvent.change(loginInput, { target: { value: loginData.email } });
-    fireEvent.change(passwordInput, { target: { value: loginData.password } });
-    const submitButton = screen.getByTitle("submitButton");
-    fireEvent.click(submitButton);
-    const errorInfo = await screen.findByText("Bad login or password!");
-    expect(errorInfo).toBeInTheDocument();
   });
 });
