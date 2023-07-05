43fdaf8e8b0c76c14348d1fbd842dd07cd9bde7a
ajgoras <igorjonczyk99@gmail.com>
2023-07-05 12:58:31 +0200

    created defaultLoggedUserState

--- src/types/LoggedUser.ts
+++ src/types/LoggedUser.ts
@@ -4,3 +4,10 @@ export interface LoggedUser {
   company_name: string;
   logo: string;
 }
+
+export const defaultLoggedUserState: LoggedUser = {
+  email: "",
+  password: "",
+  company_name: "",
+  logo: "",
+};
