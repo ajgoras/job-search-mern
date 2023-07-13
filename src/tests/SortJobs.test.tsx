895c22f2ed4b2683bb10d69fd639008f2afd876f
ajgoras <igorjonczyk99@gmail.com>
2023-07-13 15:39:12 +0200

    added sorting jobs by highest salary test

--- src/tests/SortJobs.test.tsx
+++ src/tests/SortJobs.test.tsx
@@ -6,6 +6,40 @@ import { router } from "../routes/router";
 import { getOnlyNumbersFromString } from "../functions/getOnlyNumbersFromString";
 
 describe("Should sort job offers by selected option", () => {
+  test("should sort job offers by highest salary", async () => {
+    render(
+      <ContextsProvider>
+        <RouterProvider router={router} />
+      </ContextsProvider>
+    );
+
+    const jobBarContainer = screen.getByTitle("jobBarContainer");
+    expect(jobBarContainer).toBeInTheDocument();
+
+    const sortingSelect = screen.getByTitle("sortingSelect");
+    let options: HTMLOptionElement[] = screen.getAllByTitle(
+      "sortingSelect-option"
+    );
+
+    const salaries: Number[] = [];
+    const salaryElements = await screen.findAllByTitle("salary");
+
+    fireEvent.change(sortingSelect, { target: { value: "highest salary" } });
+    expect(options[0].selected).toBeFalsy();
+    expect(options[1].selected).toBeFalsy();
+    expect(options[2].selected).toBeTruthy();
+    expect(options[3].selected).toBeFalsy();
+
+    salaryElements.forEach((salary) => {
+      salaries.push(Number(getOnlyNumbersFromString(salary.innerHTML)));
+    });
+
+    const isSorted = (salaries: Number[]) =>
+      salaries.every((v, i, a) => !i || a[i - 1] >= v);
+
+    expect(isSorted(salaries)).toBeTruthy();
+  });
+
   test("should sort job offers by lowest salary", async () => {
     render(
       <ContextsProvider>
